7cab9b7f4f98ddab98c14f27d1c580ba7c931cf1
mohammad hussain <mohammad.hussain-ext@st.com>
2019-04-03 10:19:06 +0530

    Simple Angular + Firebase App

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyD0NBVgS5LTQPvUsC4jEk-dQKW6EYUMlP8",
+    authDomain: "hussainfirechat.firebaseapp.com",
+    databaseURL: "https://hussainfirechat.firebaseio.com",
+    projectId: "hussainfirechat",
+    storageBucket: "hussainfirechat.appspot.com",
+    messagingSenderId: "821912234538"
+  }
+};
